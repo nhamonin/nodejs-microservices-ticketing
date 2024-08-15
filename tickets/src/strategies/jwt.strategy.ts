e254902a2784222728e1b2d4cd7329aa94307071
nhamonin <nazarii.hamonin@gmail.com>
2024-08-16 02:35:10 +0300

    Move jwt secret to a separate file to be able to include it separately for each microservice without duplication; Adjust tickets module to be able to read it

---
+++ tickets/src/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Request } from 'express';
+import { AUTH_COOKIE_NAME } from '@nh_tickets/common';
+
+import { JwtPayload } from '../dto/jwt-payload.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request.cookies[AUTH_COOKIE_NAME],
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<JwtPayload> {
+    return payload;
+  }
+}
--- tickets/src/tickets.module.ts
+++ tickets/src/tickets.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import * as Joi from 'joi';
+
+import { TicketsController } from './tickets.controller';
+import { TicketsService } from './tickets.service';
+import { PrismaService } from './services/prisma.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -10,11 +16,21 @@ import * as Joi from 'joi';
       isGlobal: true,
       validationSchema: Joi.object({
         DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
       }),
     }),
     PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: config.get('JWT_EXPIRATION') },
+      }),
+    }),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [TicketsController],
+  providers: [TicketsService, PrismaService, JwtStrategy],
 })
 export class TicketsModule {}
